c4318466916d80bdd6ab2e69f02d617adf84d41c
arc <78260120+arcryi@users.noreply.github.com>
2021-09-19 18:00:25 +0100

    Optimized
    Fixed clicks not registering, fixed varying call button locations.

--- callspam.py
+++ callspam.py
@@ -1,4 +1,5 @@
-import win32api, win32con
+import pydirectinput
+import pyautogui
 from time import sleep as wait
 
 print('''
@@ -17,14 +18,10 @@ wait(0.25)
 print("Warning, you cannot stop the program until all the calls have been completed.")
 missedcalls = int(input("\nHow many calls would you like to spam? -> "))
 
-def callbutton(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)   
-
 for _ in range(missedcalls):
-    wait(0.2)
-    callbutton(1540,45) # first possible call button location
-    callbutton(1571,40) # second possible call button location.
-    callbutton(1230,210) # hang up as fast as possible.
-    print("Done! => Hope they mic up soon! ;)")
+    pyautogui.moveTo(1528, 41)
+    pydirectinput.doubleClick()
+    pyautogui.moveTo(1223, 212)
+    pyautogui.doubleClick()
+    pyautogui.moveTo(1571, 38)
+    pyautogui.doubleClick()
